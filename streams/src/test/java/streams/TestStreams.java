b9db4b7ec09cc4f60cce7e64809f2c5586e5e1e2
Francisco Quevedo <dimepaco@gmail.com>
2016-01-04 19:27:37 +0100

    Add plantilla para test.

---
+++ streams/src/test/java/streams/TestStreams.java
@@ -0,0 +1,27 @@
+package streams;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Generic java Stream test.
+ * @since 1.0
+ * @version 1.0
+ */
+public class TestStreams {
+
+    /**
+     * Set up.
+     */
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Tear down.
+     */
+    @After
+    public void tearDown() {
+    }
+
+}
